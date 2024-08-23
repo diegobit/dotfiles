565416bcbcebfa05049141d1019fcf942eeffd78
Diego Giorgini <diego.giorgini@icloud.com>
2024-08-23 15:32:08 +0200

    nvim: Don't color text after label TODO/NOTE/etc

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,5 +1,18 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = false,
+      highlight = {
+        multiline = false, -- enable multine todo comments
+        before = '', -- "fg" or "bg" or empty
+        keyword = 'wide', -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+        after = '', -- "fg" or "bg" or empty
+      },
+    },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
