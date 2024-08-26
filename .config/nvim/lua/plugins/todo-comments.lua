6119ba34e904e9b8cb33f080d6a418102fb588ef
Diego Giorgini <diego.giorgini@icloud.com>
2024-08-26 09:39:27 +0200

    nvim: Update todo-comment style

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -9,8 +9,8 @@ return {
       highlight = {
         multiline = false, -- enable multine todo comments
         before = '', -- "fg" or "bg" or empty
-        keyword = 'wide', -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-        after = '', -- "fg" or "bg" or empty
+        keyword = 'wide_fg', -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+        after = 'fg', -- "fg" or "bg" or empty
       },
     },
   },
