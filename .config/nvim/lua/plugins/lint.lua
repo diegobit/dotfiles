c819de98e44063bf82f1b28d09f4d4f072b8166e
Diego Giorgini <diego.giorgini@icloud.com>
2024-08-26 20:12:12 +0200

    nvim: better lint settings: enable yaml; add LintInfo function

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -28,16 +28,18 @@ return {
       -- }
       --
       -- You can disable the default linters by setting their filetypes to nil:
+      lint.linters_by_ft['yaml'] = { 'yamllint' }
       lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
       lint.linters_by_ft['inko'] = nil
       lint.linters_by_ft['janet'] = nil
       lint.linters_by_ft['json'] = nil
       lint.linters_by_ft['markdown'] = nil
       lint.linters_by_ft['rst'] = nil
       lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
       lint.linters_by_ft['text'] = nil
+      lint.linters_by_ft['markdown'] = nil
+      lint.linters_by_ft['dockerfile'] = nil
+      lint.linters_by_ft['terraform'] = nil
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
@@ -48,6 +50,18 @@ return {
           lint.try_lint()
         end,
       })
+
+      -- Show linters for the current buffer's file type
+      vim.api.nvim_create_user_command('LintInfo', function()
+        local filetype = vim.bo.filetype
+        local linters = require('lint').linters_by_ft[filetype]
+
+        if linters then
+          print('Linters for ' .. filetype .. ': ' .. table.concat(linters, ', '))
+        else
+          print('No linters configured for filetype: ' .. filetype)
+        end
+      end, {})
     end,
   },
 }
