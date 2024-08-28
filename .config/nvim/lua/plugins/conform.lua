1f740379f0692f7d9ff3bf474c1138eb26965831
Diego Giorgini <diego.giorgini@icloud.com>
2024-08-28 10:32:11 +0200

    nvim: add json formatter to conform; disable format on save

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -11,23 +11,25 @@ return {
           require('conform').format { async = true, lsp_fallback = true }
         end,
         mode = '',
-        desc = '[Code] [F]ormat',
+        desc = '[C]ode [F]ormat',
       },
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
+      format_on_save = false,
+      -- function(bufnr)
+      -- -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- -- have a well standardized coding style. You can add additional
+      -- -- languages here or re-enable it for the disabled ones.
+      -- local disable_filetypes = { c = true, cpp = true, json = true }
+      -- return {
+      --   timeout_ms = 500,
+      --   lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      -- }
+      -- end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        json = { 'jq' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
