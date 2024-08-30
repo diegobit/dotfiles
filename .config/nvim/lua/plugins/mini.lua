3a0f30b952704c6a45a7fb1011e53cc72353f611
Diego Giorgini <diego.giorgini@icloud.com>
2024-08-30 19:40:22 +0200

    nvim: add tab number to statusline

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -46,6 +46,32 @@ return {
       statusline.section_searchcount = function()
         return ''
       end
+
+      -- Add tab number to statusline. section_filename logic from source
+      local function tab_text(is_truncated)
+        local title = is_truncated and '' or 'Tab:'
+        local tab_max = vim.fn.tabpagenr '$'
+        if tab_max > 1 then
+          return '[' .. title .. vim.fn.tabpagenr() .. '/' .. tab_max .. '] '
+        else
+          return ''
+        end
+      end
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_filename = function(args)
+        -- In terminal always use plain name
+        if vim.bo.buftype == 'terminal' then
+          return tab_text(false) .. '%t'
+        elseif MiniStatusline.is_truncated(args.trunc_width) then
+          -- File name with 'truncate', 'modified', 'readonly' flags
+          -- Use relative path if truncated
+          return tab_text(true) .. '%f%m%r'
+        else
+          -- Use fullpath if not truncated
+          return tab_text(false) .. '%F%m%r'
+        end
+      end
     end,
   },
 }
