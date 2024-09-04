6cefd363200dd7f73297f3d9ffd6ae9702754140
Diego Giorgini <diego.giorgini@icloud.com>
2024-09-04 19:07:34 +0200

    nvim: set decipher keymaps
    - with g... (like upper/lower case) do direct encodings
    - with <leader>c do encodings that ask for the encoding sccheme

--- .config/nvim/lua/plugins/decipher.lua
+++ .config/nvim/lua/plugins/decipher.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     local decipher = require 'decipher'
     decipher.setup {
-      vim.keymap.set({ 'n', 'v' }, '<leader>ube', function()
+      active_codecs = 'all',
       float = {
         border = {
           { '┌', 'FloatBorder' },
@@ -17,26 +17,50 @@ return {
           { '└', 'FloatBorder' },
           { '│', 'FloatBorder' },
         },
+
+        mappings = {
+          close = 'q',
+          apply = 'y',
+          jsonpp = 'J', -- Key to prettily format contents as json if possbile
+          help = '?',
+        },
+        enter = true,
+        options = {},
+      },
+      vim.keymap.set('n', '<leader>ce', function()
+        require('decipher').encode_motion_prompt { preview = false }
+      end, { desc = '[E]ncode with Motion' }),
+      vim.keymap.set('n', '<leader>cd', function()
+        require('decipher').decode_motion_prompt { preview = false }
+      end, { desc = '[D]ecode with Motion' }),
+      -- vim.keymap.set('v', '<leader>ce', function()
+      --   require('decipher').encode_selection_prompt { preview = true }
+      -- end, { desc = '[E]ncode Selected Text' }),
+      -- vim.keymap.set('v', '<leader>cd', function()
+      --   require('decipher').decode_selection_prompt { preview = true }
+      -- end, { desc = '[D]ecode Selected Text' }),
+      vim.keymap.set({ 'v' }, 'gbe', function()
         decipher.encode_selection 'base64'
+        local keys = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
+        vim.api.nvim_feedkeys(keys, 'm', false)
       end, { noremap = true, silent = true, desc = '[B]ase64 [E]ncode' }),
-      vim.keymap.set({ 'n', 'v' }, '<leader>ubd', function()
+      vim.keymap.set({ 'v' }, 'gbd', function()
         decipher.decode_selection 'base64'
       end, { noremap = true, silent = true, desc = '[B]ase64 [D]ecode' }),
 
-      vim.keymap.set({ 'n', 'v' }, '<leader>ubse', function()
+      vim.keymap.set({ 'v' }, 'gbse', function()
         decipher.encode_selection 'base64-url-safe'
       end, { noremap = true, silent = true, desc = '[B]ase64 URL [S]afe [E]ncode' }),
-      vim.keymap.set({ 'n', 'v' }, '<leader>ubsd', function()
+      vim.keymap.set({ 'v' }, 'gbsd', function()
         decipher.decode_selection 'base64-url-safe'
       end, { noremap = true, silent = true, desc = '[B]ase64 URL [S]afe [D]ecode' }),
 
-      vim.keymap.set({ 'n', 'v' }, '<leader>uue', function()
+      vim.keymap.set({ 'v' }, 'gue', function()
         decipher.encode_selection 'url'
       end, { noremap = true, silent = true, desc = '[U]RL [E]ncode' }),
-      vim.keymap.set({ 'n', 'v' }, '<leader>uud', function()
+      vim.keymap.set({ 'v' }, 'gud', function()
         decipher.decode_selection 'url'
       end, { noremap = true, silent = true, desc = '[U]RL [D]ecode' }),
     }
   end,
 }
-
