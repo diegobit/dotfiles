ea3b29fed8f453d67acd6c4a832835fb4a1cec1a
Diego Giorgini <diego.giorgini@icloud.com>
2024-09-07 12:19:12 +0200

    nvim: neocodeium keymaps based on C to not conflict with ALT
    to leave alt available for inserting special chars

--- .config/nvim/lua/plugins/neocodeium.lua
+++ .config/nvim/lua/plugins/neocodeium.lua
@@ -10,11 +10,16 @@ return {
       show_label = true,
       silent = true,
     }
-    vim.keymap.set('i', '<A-y>', neocodeium.accept)
-    vim.keymap.set('i', '<A-w>', neocodeium.accept_word)
-    vim.keymap.set('i', '<A-l>', neocodeium.accept_line)
-    vim.keymap.set('i', '<A-n>', neocodeium.cycle)
-    vim.keymap.set('i', '<A-c>', neocodeium.clear)
+    vim.keymap.set('i', '<C-enter>', neocodeium.accept)
+    -- vim.keymap.set('i', '<C-W>', neocodeium.accept_word)
+    vim.keymap.set('i', '<C-l>', neocodeium.accept_line)
+    vim.keymap.set('i', '<C-]>', function()
+      neocodeium.cycle(1)
+    end)
+    vim.keymap.set('i', '<C-[>', function()
+      neocodeium.cycle(-1)
+    end)
+    vim.keymap.set('i', '<C-q>', neocodeium.clear)
 
     vim.keymap.set('n', '<leader>tc', '<Cmd>NeoCodeium disable<CR>', { desc = 'neo[C]odeium' })
   end,
