9fd1cf3114b4cb2b82ea24c931c2f1732aa921cd
Diego Giorgini <diego.giorgini@icloud.com>
2024-09-07 12:36:33 +0200

    nvim: add neocodeium alt keymaps as comment

--- .config/nvim/lua/plugins/neocodeium.lua
+++ .config/nvim/lua/plugins/neocodeium.lua
@@ -1,4 +1,4 @@
--- neocodeium
+-- Neocodeium: unofficial implementation of codeium (Exafunction/codeium.nvim)
 
 return {
   'monkoose/neocodeium',
@@ -10,8 +10,9 @@ return {
       show_label = true,
       silent = true,
     }
+
+    -- Keymaps based on CTRL, to not conflict with alt used to input special chars
     vim.keymap.set('i', '<C-enter>', neocodeium.accept)
-    -- vim.keymap.set('i', '<C-W>', neocodeium.accept_word)
     vim.keymap.set('i', '<C-l>', neocodeium.accept_line)
     vim.keymap.set('i', '<C-]>', function()
       neocodeium.cycle(1)
@@ -20,6 +21,13 @@ return {
       neocodeium.cycle(-1)
     end)
     vim.keymap.set('i', '<C-q>', neocodeium.clear)
+
+    -- Keymaps based on Alt, to be intuitive, but just using a different modifier
+    -- vim.keymap.set('i', '<A-y>', neocodeium.accept)
+    -- vim.keymap.set('i', '<A-w>', neocodeium.accept_word)
+    -- vim.keymap.set('i', '<A-l>', neocodeium.accept_line)
+    -- vim.keymap.set('i', '<A-n>', neocodeium.cycle)
+    -- vim.keymap.set('i', '<A-c>', neocodeium.clear)
 
     vim.keymap.set('n', '<leader>tc', '<Cmd>NeoCodeium disable<CR>', { desc = 'neo[C]odeium' })
   end,
