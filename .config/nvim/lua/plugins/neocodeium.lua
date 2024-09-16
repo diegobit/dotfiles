bda60eab65f68c590c2fc31294cd4823954c356e
Diego Giorgini <diego.giorgini@icloud.com>
2024-09-16 18:15:20 +0200

    nvim: add codeium keymaps based on CTRL-SPACE

--- .config/nvim/lua/plugins/neocodeium.lua
+++ .config/nvim/lua/plugins/neocodeium.lua
@@ -13,13 +13,14 @@ return {
 
     -- Keymaps based on CTRL, to not conflict with alt used to input special chars
     -- WARN: <C-[> cannot be remapped! Remapping it means remapping <Esc> !
-    vim.keymap.set('i', '<C-enter>', neocodeium.accept)
-    vim.keymap.set('i', '<C-l>', neocodeium.accept_line)
-    vim.keymap.set('i', '<C-]>', function()
+    vim.keymap.set('i', '<C-space>y', neocodeium.accept)
+    vim.keymap.set('i', '<C-space>l', neocodeium.accept_line)
+    vim.keymap.set('i', '<C-space>w', neocodeium.accept_word)
+    vim.keymap.set('i', '<C-space>n', neocodeium.cycle)
+    vim.keymap.set('i', '<C-space>N', function()
       neocodeium.cycle(-1)
     end)
-    vim.keymap.set('i', '<C-q>', neocodeium.clear)
-    -- probably this map could have a better use
+    vim.keymap.set('i', '<C-space>c', neocodeium.clear)
 
     -- Keymaps based on Alt, to be intuitive, but just using a different modifier
     -- vim.keymap.set('i', '<A-y>', neocodeium.accept)
