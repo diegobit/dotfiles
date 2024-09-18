c7e36fd3300095dc6a6c07dcf72face9642ee18b
Diego Giorgini <diego.giorgini@icloud.com>
2024-09-18 13:38:51 +0200

    nvim: formatting

--- .config/nvim/lua/plugins/vim-fugitive.lua
+++ .config/nvim/lua/plugins/vim-fugitive.lua
@@ -1,6 +1,6 @@
 return {
   'tpope/vim-fugitive',
-  event = "VeryLazy",
+  event = 'VeryLazy',
   -- config = function()
   --   vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = '[G]it [S]tatus' })
   -- end
