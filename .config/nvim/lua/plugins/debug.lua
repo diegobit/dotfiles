76b5213af9a34a9ae99b1da5a005d0c57835dea6
Diego Giorgini <diego.giorgini@icloud.com>
2024-09-23 18:42:19 +0200

    nvim: better debug bindings

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -1,23 +1,11 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
-
 return {
-  -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
-
     -- Required dependency for nvim-dap-ui
     'nvim-neotest/nvim-nio',
-
-    -- Installs the debug adapters for you
+    -- Installs the debug adapters
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
 
@@ -28,16 +16,15 @@ return {
     local dap = require 'dap'
     local dapui = require 'dapui'
     return {
-      -- Basic debugging keymaps, feel free to change to your liking!
-      { '<leader>ds', dap.continue, desc = '[S]tart/Continue (or F5)' },
+      { '<leader>cc', dap.continue, desc = 'Start/[C]ontinue (F5)' },
       { '<F5>', dap.continue, desc = 'Debug: Start/Continue' },
-      { '<leader>di', dap.step_into, desc = 'Step [I]nto (or F1)' },
+      { '<leader>ci', dap.step_into, desc = 'Step [I]nto (F1)' },
       { '<F1>', dap.step_into, desc = 'Debug: Step Into' },
-      { '<leader>dv', dap.step_over, desc = 'Step o[V]er (or F2)' },
+      { '<leader>cv', dap.step_over, desc = 'Step o[V]er (F2)' },
       { '<F2>', dap.step_over, desc = 'Debug: Step Over' },
-      { '<leader>do', dap.step_out, desc = 'Step [O]ut (or F3)' },
+      { '<leader>co', dap.step_out, desc = 'Step [O]ut (F3)' },
       { '<F3>', dap.step_out, desc = 'Debug: Step Out' },
-      { '<leader>dt', dap.toggle_breakpoint, desc = '[T]oggle Breakpoint' },
+      { '<leader>cb', dap.toggle_breakpoint, desc = 'Toggle [B]reakpoint' },
       {
         '<leader>db',
         function()
@@ -45,8 +32,9 @@ return {
         end,
         desc = 'Set [B]reakpoint',
       },
-      -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-      { '<leader>dl', dapui.toggle, desc = 'See [L]ast session result.' },
+      -- Toggle to see last session result. Without this, you can't see
+      -- session output in case of unhandled exception.
+      { '<leader>cl', dapui.toggle, desc = 'See [L]ast session result (F7)' },
       { '<F7>', dapui.toggle, desc = 'Debug: See last session result.' },
       unpack(keys),
     }
@@ -64,8 +52,6 @@ return {
       -- see mason-nvim-dap README for more information
       handlers = {},
 
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
@@ -75,9 +61,6 @@ return {
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
     dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
       controls = {
         icons = {
@@ -100,11 +83,7 @@ return {
 
     -- Install golang specific config
     require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
+      delve = {},
     }
   end,
 }
