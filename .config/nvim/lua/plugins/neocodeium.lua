9f2948cc92cc0adb5219a181039db705a131f37f
Diego Giorgini <diego.giorgini@icloud.com>
2024-09-25 15:02:38 +0200

    nvim: add alternative codeium bindinds with C-

--- .config/nvim/lua/plugins/neocodeium.lua
+++ .config/nvim/lua/plugins/neocodeium.lua
@@ -12,21 +12,28 @@ return {
     }
 
     -- WARN: <C-[> cannot be remapped! Remapping it means remapping <Esc> !
-    vim.keymap.set('i', '<C-space>y', neocodeium.accept, { desc = 'Accept suggestion (Neocodeium)' })
-    vim.keymap.set('i', '<C-space>l', neocodeium.accept_line, { desc = 'Accept line (Neocodeium)' })
-    vim.keymap.set('i', '<C-space>w', neocodeium.accept_word, { desc = 'Accept word (Neocodeium)' })
-    vim.keymap.set('i', '<C-space>n', neocodeium.cycle, { desc = 'Next suggestion (Neocodeium)' })
-    vim.keymap.set('i', '<C-space>p', function()
-      neocodeium.cycle(-1)
-    end, { desc = 'Previous suggestion (Neocodeium)' })
-    -- similar to nvim cmp! show/hide
-    vim.keymap.set('i', '<C-space>k', function()
-      if neocodeium.visible() then
-        neocodeium.clear()
-      else
-        neocodeium.cycle_or_complete()
-      end
-    end, { desc = 'Show/hide suggestion (Neocodeium)' })
+
+    -- NOTE: Full set of bindings with <C-space>KEY
+    -- vim.keymap.set('i', '<C-space>y', neocodeium.accept, { desc = 'Accept suggestion (Neocodeium)' })
+    -- vim.keymap.set('i', '<C-space>l', neocodeium.accept_line, { desc = 'Accept line (Neocodeium)' })
+    -- vim.keymap.set('i', '<C-space>w', neocodeium.accept_word, { desc = 'Accept word (Neocodeium)' })
+    -- vim.keymap.set('i', '<C-space>n', neocodeium.cycle, { desc = 'Next suggestion (Neocodeium)' })
+    -- vim.keymap.set('i', '<C-space>p', function()
+    --   neocodeium.cycle(-1)
+    -- end, { desc = 'Previous suggestion (Neocodeium)' })
+    -- -- similar to nvim cmp! show/hide
+    -- vim.keymap.set('i', '<C-space>k', function()
+    --   if neocodeium.visible() then
+    --     neocodeium.clear()
+    --   else
+    --     neocodeium.cycle_or_complete()
+    --   end
+    -- end, { desc = 'Show/hide suggestion (Neocodeium)' })
+
+    -- NOTE: Reduced set of bindings: quicker, just <C-KEY> bindings that should be empty in insert mode
+    vim.keymap.set('i', '<C-g>', neocodeium.accept, { desc = 'Accept suggestion (Neocodeium)' })
+    vim.keymap.set('i', '<C-x>', neocodeium.clear, { desc = 'Clear suggestion (Neocodeium)' })
+    vim.keymap.set('i', '<C-]>', neocodeium.cycle, { desc = 'Next suggestion (Neocodeium)' })
 
     vim.keymap.set('n', '<C-space>ta', '<Cmd>NeoCodeium disable<CR>', { desc = '[A]I suggestions (Neocodeium)' })
   end,
