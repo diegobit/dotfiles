93509e24068b89aff5693945b616793808272a43
Diego Giorgini <diego.giorgini@icloud.com>
2024-09-25 15:00:21 +0200

    nvim: fix bullets keymaps

--- .config/nvim/lua/plugins/bullets.lua
+++ .config/nvim/lua/plugins/bullets.lua
@@ -1,25 +1,27 @@
+local text_ft = { 'text', 'markdown' }
+
 return {
   'bullets-vim/bullets.vim',
-  event = 'VeryLazy',
+  ft = text_ft,
+  event = 'BufReadPre',
   version = '*',
+  keys = {
+    { '<CR>', '<Plug>(bullets-newline)', mode = 'i', ft = text_ft },
+    { '<C-CR>', '<Plug>(bullets-newline)', mode = 'i', ft = text_ft },
+    { 'o', '<Plug>(bullets-newline)', mode = 'n', ft = text_ft },
+    { 'gR', '<Plug>(bullets-renumber)', mode = { 'n', 'v' }, desc = 'Renumber paragraph', ft = text_ft },
+    { 'gx', '<Plug>(bullets-toggle-checkbox)', mode = 'n', desc = 'Toggle checkbox', ft = text_ft },
+    { '<C-t>', '<Plug>(bullets-demote)', mode = 'i', desc = 'Demote bullet', ft = text_ft },
+    -- TODO: commented, as cmp breaks this
+    -- { '<C-d>', '<Plug>(bullets-promote)', mode = 'i', desc = 'Promote bullet', ft = text_ft },
+    { '>>', '<Plug>(bullets-demote)', mode = 'n', desc = 'Demote bullet', ft = text_ft },
+    { '<<', '<Plug>(bullets-promote)', mode = 'n', desc = 'Promote bullet', ft = text_ft },
+    { '>', '<Plug>(bullets-demote)', mode = 'v', desc = 'Demote bullet', ft = text_ft },
+    { '<', '<Plug>(bullets-promote)', mode = 'v', desc = 'Promote bullet', ft = text_ft }
+  },
   init = function()
     vim.g.bullets_set_mappings = 0
-  end,
-  config = function()
-    vim.keymap.set('i', '<CR>', '<Plug>(bullets-newline)')
-    vim.keymap.set('i', '<C-CR>', '<CR>')
-    vim.keymap.set('n', 'o', '<Plug>(bullets-newline)')
-
-    vim.keymap.set('n', 'gN', '<Plug>(bullets-renumber)', { desc = 'Renumber paragraph' })
-    vim.keymap.set('v', 'gN', '<Plug>(bullets-renumber)', { desc = 'Renumber' })
-
-    vim.keymap.set('n', 'gx', '<Plug>(bullets-toggle-checkbox)', { desc = 'Toggle checkbox' })
-
-    vim.keymap.set('i', '<C-t>', '<Plug>(bullets-demote)')
-    vim.keymap.set('n', '>>', '<Plug>(bullets-demote)', { desc = 'Demote bullet' })
-    vim.keymap.set('v', '>', '<Plug>(bullets-demote)')
-    vim.keymap.set('i', '<C-d>', '<Plug>(bullets-promote)')
-    vim.keymap.set('n', '<<', '<Plug>(bullets-promote)', { desc = 'Promote bullet' })
-    vim.keymap.set('v', '<', '<Plug>(bullets-promote)')
+    -- default: ['ROM', 'ABC', 'num', 'abc', 'rom', 'std-', 'std*', 'std+']
+    vim.g.bullets_outline_levels = {'ROM', 'ABC', 'num', 'abc', 'rom', 'std-'}
   end,
 }
