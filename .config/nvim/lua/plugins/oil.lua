903a56a7ea3c2d221a64971fe0eea50db57c6f9c
Diego Giorgini <diego.giorgini@icloud.com>
2024-09-26 13:09:58 +0200

    nvim: load oil on demand

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,7 @@
 return {
   'stevearc/oil.nvim',
   opts = {},
-  event = 'VimEnter',
+  event = 'VeryLazy',
   -- Optional dependencies
   -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
