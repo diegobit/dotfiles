b9eb0d0dba12fce487b169d632f909ab9ab74648
Diego Giorgini <diego.giorgini@icloud.com>
2024-09-27 15:33:16 +0200

    nvim: outline darker background

--- .config/nvim/lua/plugins/outline.lua
+++ .config/nvim/lua/plugins/outline.lua
@@ -8,7 +8,13 @@ return {
   },
   opts = {
     outline_window = {
+      winhl = 'Normal:OutlineDark',
       width = 20,
     },
   },
+  config = function(_, opts)
+    require('outline').setup(opts)
+    -- Darker background, same color as trouble
+    vim.api.nvim_set_hl(0, 'OutlineDark', { bg = '#16161e' })
+  end,
 }
