698652e7969d8a865e02b699a5eece26df67fb27
Diego Giorgini <diego.giorgini@icloud.com>
2024-10-14 15:32:25 +0200

    nvim: formatting

--- .config/nvim/lua/auto-commands.lua
+++ .config/nvim/lua/auto-commands.lua
@@ -27,18 +27,18 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 -- Integration with kitty. kitty will read this variable to know nvim is in focus
-vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
-    group = vim.api.nvim_create_augroup("KittySetVarVimEnter", { clear = true }),
-    callback = function()
-        io.stdout:write("\x1b]1337;SetUserVar=in_editor=MQo\007")
-    end,
+vim.api.nvim_create_autocmd({ 'VimEnter', 'VimResume' }, {
+  group = vim.api.nvim_create_augroup('KittySetVarVimEnter', { clear = true }),
+  callback = function()
+    io.stdout:write '\x1b]1337;SetUserVar=in_editor=MQo\007'
+  end,
 })
 
-vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
-    group = vim.api.nvim_create_augroup("KittyUnsetVarVimLeave", { clear = true }),
-    callback = function()
-        io.stdout:write("\x1b]1337;SetUserVar=in_editor\007")
-    end,
+vim.api.nvim_create_autocmd({ 'VimLeave', 'VimSuspend' }, {
+  group = vim.api.nvim_create_augroup('KittyUnsetVarVimLeave', { clear = true }),
+  callback = function()
+    io.stdout:write '\x1b]1337;SetUserVar=in_editor\007'
+  end,
 })
 
 ---- example custom lint check for rust
