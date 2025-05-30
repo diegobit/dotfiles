0606244b5ee96a72bfa1f638d035b516955611cd
Diego Giorgini <diego.giorgini@icloud.com>
2025-05-30 23:40:05 +0200

    nvim: typo

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -85,7 +85,7 @@ return {
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map('gy', require('telescope.builtin').lsp_type_definitions, 'Goto tYpe Definition')
+          map('gy', require('telescope.builtin').lsp_type_definitions, 'Goto Type Definition')
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
