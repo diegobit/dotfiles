8d55f8ecc806d074c1894c5ca48154d3715b62aa
Diego Giorgini <diego.giorgini@icloud.com>
2025-07-23 15:21:13 +0200

    nvim: remove branch from telescope, as fixes for neovim 0.11 are only on main

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
+    -- branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
       {
