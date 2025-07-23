d2f650cac664e135ceefd855912a21e34bb97d4c
Diego Giorgini <diego.giorgini@icloud.com>
2025-07-23 15:20:14 +0200

    nvim: add back TODO/etc highlight

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -10,7 +10,7 @@ return {
       highlight = {
         multiline = false, -- enable multine todo comments
         before = '', -- "fg" or "bg" or empty
-        keyword = 'wide_fg', -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+        keyword = 'wide_bg', -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
         after = '', -- "fg" or "bg" or empty
       },
     },
