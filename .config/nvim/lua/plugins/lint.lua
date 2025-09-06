2f22af06571c9eb813f8d4ca70f7d7e2a4a4c716
Diego Giorgini <diegobit@icloud.com>
2025-09-06 18:35:52 +0200

    nvim: disable dockerfile lint

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -40,7 +40,7 @@ return {
       lint.linters_by_ft['ruby'] = nil
       lint.linters_by_ft['text'] = nil
       lint.linters_by_ft['markdown'] = nil
-      lint.linters_by_ft['dockerfile'] = { 'hadolint' }
+      lint.linters_by_ft['dockerfile'] = nil
       lint.linters_by_ft['terraform'] = { 'tflint' }
 
       -- Create autocommand which carries out the actual linting
