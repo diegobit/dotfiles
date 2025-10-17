e98a2421cf45d9bb1219e72ee1e2efe6f0583a56
Diego Giorgini <diegobit@icloud.com>
2025-10-17 17:30:38 +0200

    nvim: lazier luasnip+friendly_snippets

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -3,20 +3,7 @@ return {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          {
-            'rafamadriz/friendly-snippets',
-            config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
-            end,
-          },
-        },
-      },
+      'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,11 +1,22 @@
 return {
   'L3MON4D3/LuaSnip',
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  event = 'VeryLazy',
+  build = (function()
+    return 'make install_jsregexp'
+  end)(),
+  dependencies = {
+    {
+      'rafamadriz/friendly-snippets',
+      config = function()
+        require('luasnip.loaders.from_vscode').lazy_load()
+      end,
+    },
+  },
   config = function()
     -- Load custom Lua snippets
     require('luasnip.loaders.from_lua').load {
       paths = '~/.config/nvim/lua/snippets',
     }
-    print("LuaSnip custom snippets loaded!")
+    -- print("LuaSnip custom snippets loaded!")
   end,
 }
