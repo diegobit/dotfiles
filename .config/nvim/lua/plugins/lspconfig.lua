4984136cf2837ca222749da09e7b03fbb8155ab4
Diego Giorgini <diegobit@icloud.com>
2025-11-23 11:31:33 +0100

    nvim: load lsp lazily

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -16,6 +16,7 @@ return {
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
+    event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
@@ -212,6 +213,7 @@ return {
         pyright = {
           capabilities = base_cap,
         },
+        -- ty = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
