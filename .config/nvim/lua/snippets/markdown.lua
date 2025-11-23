0f56409738fc5f090f8fe3ddbc05e7a5dd5e6f1d
Diego Giorgini <diegobit@icloud.com>
2025-11-23 11:29:45 +0100

    nvim: add markdown snippets

---
+++ .config/nvim/lua/snippets/markdown.lua
@@ -0,0 +1,20 @@
+local ls = require 'luasnip'
+local s, f, i = ls.snippet, ls.function_node, ls.insert_node
+local fmt = require('luasnip.extras.fmt').fmt
+
+ls.add_snippets('all', {
+  s(
+    { trig = 'cb', dscr = 'Empty codeblock.' },
+    fmt([[```{}```]], {
+      i(0),
+    })
+  ),
+})
+
+ls.add_snippets('all', {
+  s({ trig = 'bt', dscr = 'Add triple backticks.' }, {
+    f(function()
+      return '```'
+    end),
+  }),
+})
