bc01bfc97827afc9cd0a699e028bbd52400b5413
Diego Giorgini <diegobit@icloud.com>
2025-11-23 11:31:46 +0100

    nvim: load treesitter lazily

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    event = { 'BufReadPost', 'BufNewFile' },
     opts = {
       ensure_installed = {
         'bash',
