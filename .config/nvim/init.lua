2de1b9546ffa52497b8aa39c868351dbee5b3e42
Diego Giorgini <diegobit@icloud.com>
2025-11-23 11:35:39 +0100

    nvim, vscode: use neovim extension in vscode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -33,11 +33,17 @@ require 'keymaps'
 -- [[ Basic Autocommands ]]
 require 'auto-commands'
 
--- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+if vim.g.vscode then
+  -- VSCode extension. Disable Lazy and all extensions
+else
+  -- Ordinary Neovim
 
--- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+  -- [[ Install `lazy.nvim` plugin manager ]]
+  require 'lazy-bootstrap'
+
+  -- [[ Configure and install plugins ]]
+  require 'lazy-plugins'
+end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
