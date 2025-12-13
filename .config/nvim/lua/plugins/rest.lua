ecd26f4e762d003199c404d057b0a3719770b859
Diego Giorgini <diegobit@icloud.com>
2025-12-13 23:27:14 +0100

    nvim: fix rest keymap to run

--- .config/nvim/lua/plugins/rest.lua
+++ .config/nvim/lua/plugins/rest.lua
@@ -2,6 +2,6 @@ return {
   'rest-nvim/rest.nvim',
   event = 'BufRead *.http',
   config = function()
-    vim.keymap.set('n', '<leader>cs', '<Cmd>Rest run<CR>', { desc = 'Send REST Request' })
+    vim.keymap.set('n', '<leader>ds', '<Cmd>Rest run<CR>', { desc = 'Send REST Request (:Rest run)' })
   end,
 }
