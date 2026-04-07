3180b4ff6ae802cb8823c452591ebc998ae22785
Diego Giorgini <diegobit@icloud.com>
2026-04-07 18:53:17 +0200

    nvim: migrate nvim-treesitter for nvim 0.12

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,15 @@
 return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false,
     build = ':TSUpdate',
-    event = { 'BufReadPost', 'BufNewFile' },
-    opts = {
-      ensure_installed = {
+    config = function()
+      local filetypes = {
         'bash',
         'diff',
         'lua',
         'markdown',
-        'markdown_inline',
         'vim',
         'python',
         'ruby',
@@ -17,28 +17,19 @@ return {
         'go',
         'javascript',
         'typescript',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      matchup = { enable = true, include_match_words = true },
-      highlight = {
-        enable = true,
-        -- disable highlighting for big files
-        disable = function(lang, bufnr)
-          return lang == 'json' and vim.api.nvim_buf_line_count(bufnr) > 50000
-        end,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true },
-    },
-    config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+      }
 
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
+      require('nvim-treesitter').setup {
+        install_dir = vim.fn.stdpath('data') .. '/site',
+      }
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('kickstart-treesitter-start', { clear = true }),
+        pattern = filetypes,
+        callback = function(event)
+          pcall(vim.treesitter.start, event.buf)
+        end,
+      })
 
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
