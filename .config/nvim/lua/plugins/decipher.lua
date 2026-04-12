5ee37582ba9533aa9d051a070c1a9b48d391c967
Diego Giorgini <diegobit@icloud.com>
2026-04-12 21:20:09 +0200

    nvim: decipher: only keep general <leader> keymaps; make it lazy

--- .config/nvim/lua/plugins/decipher.lua
+++ .config/nvim/lua/plugins/decipher.lua
@@ -1,47 +1,28 @@
 -- Define keymaps here so that we can load the plugin lazily
 
----- Broken for now
--- vim.keymap.set('n', '<leader>ce', function()
---   require('decipher').encode_motion_prompt { preview = false }
--- end, { desc = 'Encode (w/ motion)' })
--- vim.keymap.set('n', '<leader>cd', function()
---   require('decipher').decode_motion_prompt { preview = false }
--- end, { desc = 'Decode (w/ motion)' })
--- vim.keymap.set('v', '<leader>ce', function()
---   require('decipher').encode_selection_prompt { preview = true }
--- end, { desc = 'Encode Selected Text' }),
--- vim.keymap.set('v', '<leader>cd', function()
---   require('decipher').decode_selection_prompt { preview = true }
--- end, { desc = 'Decode Selected Text' }),
-
-vim.keymap.set({ 'v' }, 'gbe', function()
-  require('decipher').encode_selection ('base64', { preview = false })
-  local keys = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
-  vim.api.nvim_feedkeys(keys, 'm', false)
-end, { noremap = true, silent = true, desc = 'Encode' })
-vim.keymap.set({ 'v' }, 'gbd', function()
-  require('decipher').decode_selection ('base64', { preview = false })
-end, { noremap = true, silent = true, desc = 'Decode' })
-
-vim.keymap.set({ 'v' }, 'gbse', function()
-  require('decipher').encode_selection ('base64-url-safe', { preview = false })
-end, { noremap = true, silent = true, desc = 'Encode' })
-vim.keymap.set({ 'v' }, 'gbsd', function()
-  require('decipher').decode_selection ('base64-url-safe', { preview = false })
-end, { noremap = true, silent = true, desc = 'Decode' })
-
-vim.keymap.set({ 'v' }, 'g/e', function()
-  require('decipher').encode_selection ('url', { preview = false })
-end, { noremap = true, silent = true, desc = 'Encode' })
-vim.keymap.set({ 'v' }, 'g/d', function()
-  require('decipher').decode_selection ('url', { preview = false })
-end, { noremap = true, silent = true, desc = 'Decode' })
+vim.keymap.set('n', '<leader>ce', function()
+  require('decipher').encode_motion_prompt { preview = false }
+end, { desc = 'Encode (w/ motion)' })
+vim.keymap.set('n', '<leader>cd', function()
+  require('decipher').decode_motion_prompt { preview = false }
+end, { desc = 'Decode (w/ motion)' })
+vim.keymap.set('x', '<leader>ce', function()
+  require('decipher').encode_selection_prompt { preview = false }
+end, { desc = 'Encode Selected Text' })
+vim.keymap.set('x', '<leader>cd', function()
+  require('decipher').decode_selection_prompt { preview = false }
+end, { desc = 'Decode Selected Text' })
 
 return {
   'MisanthropicBit/decipher.nvim',
-  version = '*',
-  -- event = 'VeryLazy',
+  -- version = '*',
+  branch = 'add-commands',
   lazy = true, -- Load with keymap
+  cmd = {
+    'DecipherEncode',
+    'DecipherDecode',
+    'DecipherVersion',
+  },
   config = function()
     local decipher = require 'decipher'
     decipher.setup {
